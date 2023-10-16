290ca9b5c5a09d0efbe69e33661ba6709f4213aa
Nita Youssef <nitayoussef3@gmail.com>
2023-10-16 05:11:44 -0700

    stay still -_-

--- main.h
+++ main.h
@@ -4,23 +4,15 @@
 #include <stdarg.h>
 #include <unistd.h>
 
-/* _printf */
 int _printf(const char *format, ...);
-
-/* write functions */
-int _putchar(char c);
-
-/* Custom conversion specifiers */
+int handle_integer(long num);
+int handle_long_integer(long num);
+int handle_unsigned(unsigned long num);
+int handle_unsigned_long(unsigned long num);
+int handle_base(unsigned long num, char base, int uppercase);
+int handle_pointer(void *ptr);
 int handle_string_custom(char *str);
 int handle_reverse(char *str);
 int handle_rot13(char *str);
 
-/* Helper functions for other conversion specifiers */
-int handle_integer(int num);
-int handle_binary(unsigned int num);
-int handle_unsigned(unsigned int num);
-int handle_octal(unsigned int num);
-int handle_hex(unsigned int num, int uppercase);
-int handle_pointer(void *ptr);
-
-#endif /* !MAIN_H */
+#endif /* MAIN_H */
