f8de3270aa4c989f58fced7e24eddb288113a5f0
Nita Youssef <nitayoussef3@gmail.com>
2023-10-16 14:33:42 -0700

    dd

--- _printf.c
+++ _printf.c
@@ -74,7 +74,14 @@ int _printf(const char *format, ...)
             else if (*format == 's')
             {
                 char *str = va_arg(args, char *);
-                count += handle_string(str);
+                if (!str)
+                    str = "(null)";
+                while (*str)
+                {
+                    write(1, str, 1);
+                    str++;
+                    count++;
+                }
             }
             else if (*format == 'd' || *format == 'i')
             {
@@ -375,38 +382,4 @@ int handle_rot13(char *str)
         count++;
     }
     return count;
-}
-/**
- * handle_string - prints a string
- * @str: string to be printer
- * Return: length of the string
-*/
-{
-	char *null;
-
-	int count;
-
-	int i;
-
-	count = 0;
-
-	if (str == NULL)
-	{
-		null = "(null)";
-
-		for (i = 0; i < 6; i++)
-		{
-			_putchar(*null);
-			null++;
-		}
-		return (6);
-	}
-
-	while (*str)
-	{
-		_putchar(*str);
-		count++;
-		str++;
-	}
-	return (count);
 }
